71640ad352512f35b65fceee71185e05e745db0d
yla <officiallyyla@gmail.com>
2025-02-08 10:02:54 -0600

    sign up

---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Signup = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label> Enter email:
+                <input>
+                type = "email"
+                value = {email}
+                onChange = {e => setEmail(e.target.value)}
+                </input>
+            </label>
+            <br />
+            <label> Enter password:
+                <input>
+                type = "password"
+                value = {password}
+                onChange = {e => setPassword(e.target.value)}
+                </input>
+            </label>
+            <input> type = "submit </input>
+        </form>
+    )
+}
