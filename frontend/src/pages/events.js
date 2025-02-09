2b4c30969af71791374cb851756b20be3f1312dc
Guillermo <guillermomartinez1998@gmail.com>
2025-02-08 20:13:05 -0600

    keeping up to date with main

--- frontend/src/pages/events.js
+++ frontend/src/pages/events.js
@@ -6,11 +6,11 @@ const Event = () => {
     const navigate = useNavigate();
 
     return (
-        <div style={{display: "flex", justifyContent: "center"}}>
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "10vh" }}>
+        <div>
+            <div style={{ display: "grid", justifyContent: "center", alignItems: "center", height: "30vh"}}>
                 Test
             </div>
-            <div className="central_container" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>   
+            <div className="central_container" style={{ display: "grid", justifyContent: "center", alignItems: "center", height: "50vh"}}>   
 
                 <button onClick={() => navigate("/home")}>
                     Home
