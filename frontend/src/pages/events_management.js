208c3a60cdd812b00f5ddbc9816b844a90fb948c
Guillermo <guillermomartinez1998@gmail.com>
2025-02-13 22:49:12 -0600

    created css file for events management, added routing for e_m in app and navbar and e_m is now a working page, still empty

---
+++ frontend/src/pages/events_management.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/events_management.css";
+
+const Event_Management = () => {
+
+    const navigate = useNavigate();
+
+    return(
+        
+        <div>
+
+        </div>
+
+    );
+
+};
+
+export default Event_Management;
