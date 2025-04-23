73cba2be346cbfdb8b8669166f8b89bb25c51eab
Nicolas Mangilit <nico.mangilit@outlook.com>
2025-04-22 22:55:40 -0500

    fixed refreshing error issue

--- infra/main.tf
+++ infra/main.tf
@@ -72,51 +72,63 @@ locals {
 
 
 resource "aws_cloudfront_distribution" "kiwe_distribution" {
-    origin {
-        domain_name = aws_s3_bucket.kiwe-bucket.bucket_regional_domain_name
-        origin_id = local.s3_origin_id
+  origin {
+    domain_name = aws_s3_bucket.kiwe-bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
 
-        s3_origin_config {
-          origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
-        }
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
+  }
 
-    enabled = true
-    is_ipv6_enabled = true
-    default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
-    default_cache_behavior {
-      allowed_methods = ["GET", "HEAD"]
-      cached_methods = ["GET", "HEAD"]
-      target_origin_id = local.s3_origin_id
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-      forwarded_values {
-        query_string = false
-        cookies {
-          forward = "none"
-        }
-      }
-      viewer_protocol_policy = "redirect-to-https"
-      min_ttl = 0
-      default_ttl = 3600
-      max_ttl = 86400
-    }
-
-    price_class = "PriceClass_100"
-
-    viewer_certificate {
-      cloudfront_default_certificate = true
-    }
-
-    restrictions {
-      geo_restriction {
-        restriction_type = "whitelist"
-        locations = ["US", "CA"]
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
       }
     }
 
-    tags = {
-        project = "kiwe"
-    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA"]
+    }
+  }
+
+  tags = {
+    project = "kiwe"
+  }
 }
